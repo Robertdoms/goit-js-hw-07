a1c766d96ee084e63bcfd47ad952b60fa14c5d8f
RobertDoms <robertdoms90@gmail.com>
2022-10-09 20:32:24 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+gallery.addEventListener("click", onItemClick);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join("");
+}
+
+function onItemClick(event) {
+  gallery.addEventListener("keydown", evt => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="desc"/>`
+  );
+  const galleryItem = event.target.classList.contains("gallery__image");
+  if (!galleryItem) {
+    return;
+  } else {
+    instance.show();
+  }
+}
